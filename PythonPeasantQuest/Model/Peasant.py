48ffb13af0c098170e0e5497c2d9d68c4c42c30b
Kaitlyn Fernelius <kkin001@gmail.com>
2022-08-13 02:31:16 -0500

    dID A LOT OF SHIT I sure hope it works

--- PythonPeasantQuest/Model/Peasant.py
+++ PythonPeasantQuest/Model/Peasant.py
@@ -1,4 +1,4 @@
-#Class to create the peasant object that will be saved in a file
+#class to create the peasant object that will be used to determine game progress
 
 class Peasant: 
 
@@ -6,10 +6,8 @@ class Peasant:
         self.name = name
         self.storyframe = storyframe #(i.e. A1, A2, A3, A4)
         self.inventory = inventory
-        #Boolean status elements to determine if the peasant is ready to defeat trogdor
         self.stink = stink
         self.dress = dress
         self.fire = fire
 
-global dashing
-dashing = Peasant("Dashing", "Main", {}, False, False, False)
+#setting all the objects that the character needs to obtain
--- PythonPeasantQuest/main.py
+++ PythonPeasantQuest/main.py
@@ -1,24 +1,19 @@
 from art import *
 import os
-from travelAnimation import load_animation
-from A4 import peasantHome
-from globalInfo import complexInstructions
-
-#Game starts - describes character
-#HELP: when typed at any point shows certain commands
-#MoveTo: Allows the character to move to a certain point on the map
-#Each LOCATION: has certain objectives (that allow the story to progress)
-#Hero has items that they must obtain, if item == true then they can do a certain action
+from Utilities.travelAnimation import load_animation
+from Views.B3 import peasantHome
+from Utilities.globalInfo import complexInstructions, introText
 
 
 def main():
 
+    #initial introduction to PeasCant Quest
     tprint("PeasCant Quest", font="epic",chr_ignore=True)
     print(complexInstructions)
-    
 
     startGame = input("Are you ready to begin the quest?? y/n: ")
-     
+
+    #TODO - Grab game information from CSV file and determine if they want to load save information 
     if startGame.lower() == "y":
         os.system("clear")
         load_animation("Booting sad peasantry....")
@@ -26,12 +21,8 @@ def main():
         tprint("HOUSE", font="fire_font-s", chr_ignore=True)
         aprint("at what cost")
 
-        print("\nYou are Rather Dashing, a humble peasant living in the peasant kingdom of Peasantry\n" \
-                "You return home from a vacation on Scalding Lake only to find that Trogdor the Burninator\n" \
-                "has burninated your thatched roof cottage along with all your goods and services.\n"   
-                "With nothing left to lose, you swear to get revenge on that Wingaling Dragon\n"  
-                "You must ready yourself to head east towards the mountain atop which Trogdor lives.")
-                
+        print(introText)
+
         mv = input("\nPress ENTER to BEGIN\n")
         if(mv.lower() != "q"):
             os.system("clear")
