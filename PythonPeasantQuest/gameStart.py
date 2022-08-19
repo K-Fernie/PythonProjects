cfb5459814fa3b2463498a6b0937e58647389509
Katy Fernelius <kkin001@gmail.com>
2022-08-19 02:22:56 -0500

    updates made to gamestart

--- PythonPeasantQuest/gameStart.py
+++ PythonPeasantQuest/gameStart.py
@@ -1,7 +1,11 @@
+"""
+gameStart is where the game is initialized.
+The Game screen and the death screen are shown as needed during the game loop
+"""
 import curses
 from globalData import stringResponses,dashing, dashObjectives, dashObjectivesCopy
 from curses.textpad import Textbox
-from helperFunctions import textInteract, enter_is_terminate, getMap   
+from helperFunctions import textInteract, enter_is_terminate
 
 
 """
@@ -26,22 +30,23 @@ def deadGuy(screen):
     sub2 = sub.subwin(8, 58, 22, 2)
     tb = curses.textpad.Textbox(sub2)
     win.refresh()
-    
+
     for i in range(1,20):
         win.addstr(i,1,dashObjectives.objectiveDict[screen]["map"][i])
 
+    #Setting boolean to break the game loop
     close_screen = False
 
     while not close_screen:
         #Clearing and refreshing to capture the screen information for the text screen
         sub2.clear()
         sub2.refresh()
-        
+
         win.addstr(20, 2, f' {screen} ')
-        
+
         event = win.getch()
 
-        if event == curses.KEY_END: 
+        if event == curses.KEY_END:
             curses.beep()
             close_screen = True
         if event == curses.KEY_HOME:
@@ -62,7 +67,7 @@ def deadGuy(screen):
                     sub2.addstr("I didn't get that, would you like to play again ??:")
 
     curses.endwin()
-       
+
 """
 Initializing game play for Peasants Quest
 The goal is to get all the items needed to:
@@ -87,16 +92,16 @@ def gameStart(screen,heroloc):
     sub2 = sub.subwin(8, 58, 22, 2)
     tb = curses.textpad.Textbox(sub2)
     win.refresh()
-    
+
     #Initiating list to track the current map's obstacles
     obsList = []
     #create view and create obstacle list
     for i in range(1,20):
         win.addstr(i,1,dashObjectives.objectiveDict[screen]["map"][i])
-        for j in range(len(dashObjectives.objectiveDict[screen]["map"][i])): 
+        for j in range(len(dashObjectives.objectiveDict[screen]["map"][i])):
             if dashObjectives.objectiveDict[screen]["map"][i][j] != " ":
                 obsList.append([i,j+1])
-        
+
 
     heroImg = '\u265E'
     hero = list(heroloc)
@@ -106,11 +111,11 @@ def gameStart(screen,heroloc):
     if screen == "C4" or screen == "B1" and "belt" not in dashing.inventory:
         kerrekloc = [2,45]
         kerrek = "\(⊙ \ _ /◎)/"
-        
+
         win.refresh()
         pass
  
-    
+
     #Setting booleans for screen control
     
     down = True
@@ -121,17 +126,17 @@ def gameStart(screen,heroloc):
         #Clearing and refreshing to capture the screen information for the text screen
         sub2.clear()
         sub2.refresh()
-        
+
         win.addstr(20, 2, f' {screen} ')
 
         #Determining Exit Locations and adding exits accordingly
         northExit,westExit,southExit,eastExit = False, False, False, False
         if "north" in dashObjectives.objectiveDict[screen]["exit"]:
-            win.addstr(0, 25, ' Exit North ') #10 characters starting at 25 if x is between 25 - 30
+            win.addstr(0, 25, ' Exit North ')
             northExit = True
 
-        if "east" in dashObjectives.objectiveDict[screen]["exit"]: 
-            win.addch(7, 59, ' ')   
+        if "east" in dashObjectives.objectiveDict[screen]["exit"]:
+            win.addch(7, 59, ' ')
             win.addch(8, 59, 'E')
             win.addch(9, 59, 'a')
             win.addch(10, 59, 's')
@@ -140,7 +145,7 @@ def gameStart(screen,heroloc):
             eastExit = True
 
         if "west" in dashObjectives.objectiveDict[screen]["exit"]:
-            win.addch(7, 0, ' ')   
+            win.addch(7, 0, ' ')
             win.addch(8, 0, 'W')
             win.addch(9,0, 'e')
             win.addch(10, 0, 's')
@@ -150,15 +155,15 @@ def gameStart(screen,heroloc):
 
         if "south" in dashObjectives.objectiveDict[screen]["exit"]:
             win.addstr(20, 25, ' Exit South ')
-            southExit = True 
-        
+            southExit = True
+
         event = win.getch()
 
         y = hero[0]
         x = hero[1]
-        if screen == "Dead_Screen":     
-            deadGuy(sub2, tb) 
-        if event == curses.KEY_END: 
+        if screen == "Dead_Screen":
+            deadGuy(sub2, tb)
+        if event == curses.KEY_END:
             curses.beep()
             close_screen = True
         if event == curses.KEY_HOME:
@@ -166,16 +171,16 @@ def gameStart(screen,heroloc):
         if event == curses.KEY_DOWN:
             y += 1
             try:
-                
-                if y == 20: 
+
+                if y == 20:
                     curses.endwin()
                     gameStart(dashObjectives.objectiveDict[screen]["exit"].get("south"),[1,30])
                     break
-                elif y == 19 and (x >= 25 and x < 35) and southExit: 
+                elif y == 19 and (x >= 25 and x < 35) and southExit:
                     win.addch(hero[0], hero[1], " ")
                     hero[0] = y
                     win.addch(hero[0], hero[1], heroImg)
-                elif y == 19 or [y,x] in obsList: 
+                elif y == 19 or [y,x] in obsList:
                     pass
                 else:
                     win.addch(hero[0], hero[1], " ")
@@ -188,17 +193,17 @@ def gameStart(screen,heroloc):
             y -= 1
             try:
                 #This is the North Exit
-                if y==0: 
+                if y==0:
                     curses.endwin()
                     gameStart(dashObjectives.objectiveDict[screen]["exit"].get("north"),[19,30])
                     break
-                if y == 1 and (x >= 25 and x < 35) and northExit: 
+                if y == 1 and (x >= 25 and x < 35) and northExit:
                     win.addch(hero[0], hero[1], " ")
                     hero[0] = y
                     win.addch(hero[0], hero[1], heroImg)
-                elif y == 1 or [y,x] in obsList: 
+                elif y == 1 or [y,x] in obsList:
                     pass
-                else: 
+                else:
                     win.addch(hero[0], hero[1], " ")
                     hero[0] = y
                     win.addch(hero[0], hero[1], heroImg)
@@ -208,7 +213,7 @@ def gameStart(screen,heroloc):
         if event == curses.KEY_LEFT:
             x -= 1
             try:
-                if x == 0: 
+                if x == 0:
                     curses.endwin()
                     gameStart(dashObjectives.objectiveDict[screen]["exit"].get("west"),[10,58])
                     break
@@ -218,17 +223,17 @@ def gameStart(screen,heroloc):
                     win.addch(hero[0], hero[1], heroImg)
                 elif x == 1 or [y,x] in obsList:
                     pass
-                else: 
+                else:
                     win.addch(hero[0], hero[1], " ")
                     hero[1] = x
                     win.addch(hero[0], hero[1], heroImg)
             except:
                 pass
 
-        if event == curses.KEY_RIGHT: 
+        if event == curses.KEY_RIGHT:
             x += 1
             try:
-                if x == 59: 
+                if x == 59:
                     curses.endwin()
                     gameStart(dashObjectives.objectiveDict[screen]["exit"].get("east"),[10,1])
                     break
@@ -236,9 +241,9 @@ def gameStart(screen,heroloc):
                     win.addch(hero[0], hero[1], " ")
                     hero[1] = x
                     win.addch(hero[0], hero[1], heroImg)
-                elif x == 58 or [y,x] in obsList: 
+                elif x == 58 or [y,x] in obsList:
                     pass
-                else: 
+                else:
                     win.addch(hero[0], hero[1], " ")
                     hero[1] = x
                     win.addch(hero[0], hero[1], heroImg)
@@ -248,22 +253,22 @@ def gameStart(screen,heroloc):
 
     #KERREK SCREEN INFORMATION#
         if (screen == "C4" or screen == "B1") and "belt" not in dashing.inventory:
-            
+
             win.timeout(200)
             win.addstr(kerrekloc[0], kerrekloc[1], kerrek)
             y1=kerrekloc[0]
             x1=kerrekloc[1]
 
-            if y1 == 19: 
+            if y1 == 19:
                 down = False
-            
-            if y1 == 1: 
+
+            if y1 == 1:
                 down=True
-            
-            if x1 == 1: 
+
+            if x1 == 1:
                 left = False
-            
-            if x1 == 59: 
+
+            if x1 == 59:
                 left = True
 
             if (y1 != 19 and down) and (x1 != 1 and left):
@@ -273,12 +278,12 @@ def gameStart(screen,heroloc):
             elif (y1 != 1 and not down):
                 y1-=1
                 x1+=2
-            
+
             win.addstr(kerrekloc[0], kerrekloc[1], "            ")
             kerrekloc[0] = y1
             kerrekloc[1] = x1
             win.addstr(kerrekloc[0], kerrekloc[1], kerrek)
-            
+
             #Kerrek's Kill zone
             killZone = [
             [y1,x1-1],
@@ -296,9 +301,9 @@ def gameStart(screen,heroloc):
             [y1,x1+10]
             ]
             #Death Conditions
-          
+
             if hero in killZone:
-                close_screen = True 
+                close_screen = True
         if "sword" in dashing.inventory:
             dashObjectives.objectiveDict["E2"]["exit"]["east"] = "Trog_lair"
 
@@ -307,13 +312,8 @@ def gameStart(screen,heroloc):
 
     curses.endwin()
     if close_screen == True:
-        deadGuy("Dead_Screen")            
-#TODO - Finish writing the requirements for the JSON file 
-#TODO - Add comments
-# initiating separate from main for testing purposes only
+        deadGuy("Dead_Screen")
 
-#deadGuy("Dead_Screen") 
-#gameStart("B3",[15,5]) 
-
-            
+#deadGuy("Dead_Screen")
+#gameStart("B3",[15,5])
 
